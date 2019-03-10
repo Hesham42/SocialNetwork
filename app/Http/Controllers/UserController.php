c266c94b1a5f4f26d87a89e17fe6973447bc3df4
Hesham <hmostafa3bd@gmail.com>
2019-03-10 23:24:31 +0200

    validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,6 +15,12 @@ class UserController extends Controller
 
     public function postSignUp(Request $request)
     {
+        $this->validate($request, [
+            'firstName' => 'required|max:25',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W)/',
+        ]);
+
         $email = $request['email'];
         $firstName = $request['firstName'];
         $password = bcrypt($request['password']);
@@ -33,6 +39,10 @@ class UserController extends Controller
 
     public function postSignIn(Request $request)
     {
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
         if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
             return redirect()->route('dashboard');
         }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,22 +2,32 @@
 @section('title')
 welcom !!
 @endsection
-@section('content')
+@section('content') 
+@if (count($errors) > 0)
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
 <div class="row">
         <div class="clo-md-6" >
             <h1>Sign up</h1>
                 <form action="{{route('signup')}}" method="post">
-                        <div class="form-group">
+                        <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
                                 <label for="email">Your Email</label>
-                                <input class="form-control" type="text" name="email" id="email">
+                        <input class="form-control" type="text" name="email" id="email" value="{{ Request::old('email') }}}">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group {{ $errors->has('firstName') ? 'has-error' : '' }}">
                                     <label for="firstName">Your firstName</label>
-                                    <input class="form-control" type="text" name="firstName" id="firstName">
+                                    <input class="form-control" type="text" name="firstName" id="firstName" value="{{ Request::old('firstName') }}}">
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
                                         <label for="password">Your password</label>
-                                        <input class="form-control" type="password" name="password" id="password">
+                                        <input class="form-control" type="password" name="password" id="password" value="{{ Request::old('password') }}}">
                                     </div>
                                     <button type="submit" class="btn btn-primary">Submit</button>
                                     <input type="hidden" name="_token" value="{{ Session::token() }}">
@@ -27,13 +37,13 @@ welcom !!
                  <div class="clo-md-6" >
                     <h1>Sign In</h1>
                     <form action="{{route('signin')}}" method="post">
-                            <div class="form-group">
-                                        <label for="email">Your Email</label>
-                                        <input class="form-control" type="text" name="email" id="email">
+                            <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+                                    <label for="email">Your Email</label>
+                                        <input class="form-control" type="text" name="email" id="email" value="{{ Request::old('email') }}}">
                                     </div>
-                                        <div class="form-group">
-                                                <label for="password">Your password</label>
-                                                <input class="form-control" type="password" name="password" id="password">
+                                    <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
+                                            <label for="password">Your password</label>
+                                                <input class="form-control" type="password" name="password" id="password" value="{{ Request::old('password') }}}">
                                             </div>
                                             <button type="submit" class="btn btn-primary">Submit</button>
                                             <input type="hidden" name="_token" value="{{ Session::token() }}">
