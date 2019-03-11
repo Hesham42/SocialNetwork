41fba9924cc83d5806ea9de4b642ad78738d74d4
Hesham <hmostafa3bd@gmail.com>
2019-03-11 04:15:39 +0200

    Make validation to post Creation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,6 @@
 @extends('layout.master')
 @section('content')
+@include('include.message-block')
 <section class="row new-post">
         <div class="col-md-6 col-md-offset-3">
                 <header><h3>What do you have to say?</h3></header>
